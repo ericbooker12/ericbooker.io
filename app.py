ffad024bee39bee34595b0b2dc3407fa6a7bb74c
Eric Booker <ericbooker12@gmail.com>
2020-07-28 19:26:39 -0700

    Add Episode Picker app.

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
-
-
+def welcome():
 	return "<H1>Eric Booker</H1>"
+
+@app.route("/episode-picker", methods=["GET", "POST"])
+def episode_picker():
+    return render_template("episode-picker.html")
 
 
 if __name__ == '__main__':
