fef4213bd58c003716c5ee197be6bc79694804e8
Eric Booker <ericbooker12@gmail.com>
2020-10-24 21:10:42 -0700

    Add devices app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
-from episode_data import get_series_data, get_movie_data
+from episode_data import get_series_data, get_movie_data, get_series_data_random
 import random
 
 
@@ -19,14 +19,46 @@ def episode_picker_deluxe():
 
     return render_template("episode-picker-deluxe.html")
 
+@app.route("/episode-details", methods=["GET", "POST"])
+def episode_details():
 
-@app.route("/get-show-data", methods=["GET", "POST"])
-def get_show_data():
+    return render_template("episode-details.html")
+
+@app.route("/get-episode-details", methods=["GET", "POST"])
+def get_episode_details():
 
     if request.method == 'POST':
         show = request.form['show']
 
         show_info = get_series_data(show)
+
+        if show_info == "movie":
+
+            movie_info = get_movie_data(show)
+
+            # print(movie_info)
+
+            return render_template("movie-results.html", movie = movie_info) 
+        
+        else:
+
+            
+            return render_template(
+                "get-episode-details.html",
+                show_info = show_info
+            )
+
+    else:
+        return redirect(url_for('episode_picker_deluxe'))
+
+
+@app.route("/get-random-show", methods=["GET", "POST"])
+def get_random_show():
+
+    if request.method == 'POST':
+        show = request.form['show']
+
+        show_info = get_series_data_random(show)
 
         if show_info == "movie":
 
@@ -38,12 +70,9 @@ def get_show_data():
         
         else:
 
-            print("*************************")
-            print(show_info)
-            print("*************************")
-            
-        
             num_of_seasons = len(show_info)
+
+            # print(show_info)
             
             rand_season = random.randint(1, num_of_seasons)
 
@@ -58,7 +87,7 @@ def get_show_data():
             episode_to_watch = episode_list[random_episode - 1]
             
             return render_template(
-                "episode-results.html", 
+                "random-episode-results.html", 
                 show_title = show_title, 
                 show_info = show_info, 
                 year_released = year, 
@@ -70,6 +99,10 @@ def get_show_data():
 
     else:
         return redirect(url_for('episode_picker_deluxe'))
+
+@app.route("/devices")
+def devices():
+    return render_template("devices.html")
 
 
 if __name__ == '__main__':
--- episode_data.py
+++ episode_data.py
@@ -1,8 +1,14 @@
 from imdb import IMDb
 
-def get_series_data(show):
+def get_series_data_random(show):
   ia =IMDb()
   series = ia.search_movie(show)[0]
+  movie_id = series.movieID
+  # print(movie_id)
+  series_data = ia.get_movie(movie_id)
+
+  # outline = series_data.data['plot outline'] 
+  plot = series_data.data['plot']
 
   if series['kind'] == 'tv series':
 
@@ -11,6 +17,9 @@ def get_series_data(show):
     year = series['year']
 
     episode_data = series.data['episodes']
+    # plot = series.data['plot outline'] 2
+
+    # print(series)
     num_of_seasons = len(episode_data)
     series_data = []
     seasons = {}
@@ -22,7 +31,7 @@ def get_series_data(show):
       season_number = season_number
       num_of_episodes = len(season)
 
-      for episode in season:
+      for episode in season:  
         season_data = {}
         episode_list.append(str(season[episode]))
 
@@ -31,11 +40,68 @@ def get_series_data(show):
       season_data['season_number'] =  season_number
       season_data['number_of_episodes'] =  num_of_episodes
       season_data['episode_list'] =  episode_list
+      season_data['plot'] =  plot
+  
 
       series_data.append(season_data)
 
     # series_data will be return value
     return series_data
+  
+  elif series['kind'] == 'movie':
+    return "movie"
+
+def get_series_data(show):
+  ia =IMDb()
+  series = ia.search_movie(show)[0]
+  movie_id = series.movieID
+  series_data = ia.get_movie(movie_id)
+  cast = []
+  show_data = {}
+
+  if series['kind'] == 'tv series':
+
+    ia.update(series, 'episodes')
+
+    for cast_member in series_data['cast']:
+      cast.append(cast_member['name'])
+
+    show_data['title'] = str(series_data)
+    show_data['plot'] = series_data.data['plot'][0].split("::")[0]
+    show_data['rating'] = series_data.data['rating']
+    show_data['year'] = series_data.data['year']
+    show_data['genre'] = series_data.data['genres'][0]
+    show_data['years_active'] = series_data.data['series years']
+    show_data['cast'] = cast
+
+    episode_data = series.data['episodes']
+
+    num_of_seasons = len(episode_data)
+    series_data = []
+    seasons = {}
+
+    for season_number in episode_data:
+      episode_list = []
+
+      season_name = 'season ' + str(season_number)
+
+      # print(season_name)
+
+      season = episode_data[season_number]
+      season_number = season_number
+      num_of_episodes = len(season)
+
+      for episode in season:  
+        season_data = {}
+        episode_list.append(str(season[episode]))
+
+      seasons[season_name] = episode_list
+      
+      series_data.append(season_data)
+
+    show_data['episodes'] = seasons
+
+    return show_data
   
   elif series['kind'] == 'movie':
     return "movie"
@@ -175,3 +241,88 @@ def get_movie_data(movie):
 # <Movie id:0421030[http] title:_"Big Love" (2006)_>, 
 # <Movie id:9441638[http] title:_The Big Ugly (2020)_>]
 
+#__________________________________________________________
+
+# {'original title': 'Woke (2020- )', 
+# 'cast': [<Person id:2031358[http] name:_Lamorne Morris_>, 
+# <Person id:2981935[http] name:_Blake Anderson_>, 
+# <Person id:5968385[http] name:_T. Murph_>, 
+# <Person id:1356578[http] name:_J.B. Smoove_>, 
+# <Person id:3550886[http] name:_Sasheer Zamata_>, 
+# <Person id:0570860[http] name:_Rose McIver_>, 
+# <Person id:7596733[http] name:_Alvina August_>, 
+# <Person id:3931538[http] name:_Sam Richardson_>, 
+# <Person id:0355024[http] name:_Tony Hale_>, 
+# <Person id:3355329[http] name:_Nicole Byer_>, 
+# <Person id:0147825[http] name:_Cedric the Entertainer_>, 
+# <Person id:6297445[http] name:_Donna Benedicto_>, 
+# <Person id:1275101[http] name:_Shannon Chan-Kent_>, 
+# <Person id:1273767[http] name:_Kurt Long_>, 
+# <Person id:3651929[http] name:_Shannon Kook_>, 
+# <Person id:0838588[http] name:_Cree Summer_>, 
+# <Person id:1034338[http] name:_Elizabeth Bowen_>, 
+# <Person id:0341176[http] name:_Eddie Griffin_>, 
+# <Person id:1442113[http] name:_Jack McBrayer_>, 
+# <Person id:8409919[http] name:_Adam Kirschner_>, 
+# <Person id:1352845[http] name:_David Ury_>, 
+# <Person id:0202966[http] name:_Keith David_>, 
+# <Person id:5088392[http] name:_Justyna Bania_>, 
+# <Person id:1052534[http] name:_Nathan Lee Graham_>, 
+# <Person id:2577076[http] name:_Lil Rel Howery_>, 
+# <Person id:1022732[http] name:_Link Baker_>, 
+# <Person id:9112433[http] name:_Jarrett Carlington_>, 
+# <Person id:0150376[http] name:_Lossen Chambers_>, 
+# <Person id:0445389[http] name:_Peter Kelamis_>, 
+# <Person id:1284045[http] name:_Diana Pavlovská_>, 
+# <Person id:7856682[http] name:_Leanne Allen_>, 
+# <Person id:2390004[http] name:_Bryan Demore_>, 
+# <Person id:11885145[http] name:_Cassandra Bolinski_>, 
+# <Person id:0237943[http] name:_Jasmin Dring_>, 
+# <Person id:11401307[http] name:_Sarah Rodgers_>, 
+# <Person id:6801314[http] name:_Clare Filipow_>, 
+# <Person id:10479344[http] name:_Sam Rahmani_>, 
+# <Person id:5820794[http] name:_Lyle Reginald_>, 
+# <Person id:2686167[http] name:_Krista Magnusson_>, 
+# <Person id:4316154[http] name:_Vanessa Richards_>, 
+# <Person id:1995155[http] name:_Janet Glassford_>, 
+# <Person id:7221026[http] name:_Jacki Gunn_>, 
+# <Person id:0836071[http] name:_Wes Studi_>, 
+# <Person id:0871141[http] name:_Phil Trasolini_>, 
+# <Person id:0183576[http] name:_Chris Cound_>, 
+# <Person id:4269856[http] name:_Susan Hanson_>, 
+# <Person id:2788169[http] name:_Casey Manderson_>, 
+# <Person id:1417897[http] name:_Brandi Alexander_>, 
+# <Person id:0242151[http] name:_Herbert Duncanson_>, 
+# <Person id:11885132[http] name:_Ute Rée_>], 
+# 'genres': ['Comedy'], 
+# 'countries': ['United States'], 
+# 'country codes': ['us'], 
+# 'language codes': ['en'], 
+# 'color info': ['Color'], 
+# 'certificates': ['Canada:TV-MA::(self-applied)', 
+# 'United States:TV-MA::(self-applied)'], 
+# 'creator': [<Person id:10056664[http] name:_Keith Knight_>, 
+# <Person id:1187871[http] name:_Marshall Todd_>], 
+# 'number of seasons': 1, 
+# 'rating': 6.2, 
+# 'votes': 1642, 
+# 'cover url': 'https://m.media-amazon.com/images/M/MV5BNDIzOTJjM2QtNjkwNS00NGFhLWJhYjgtZjAxZGZmYjFlZTg4XkEyXkFqcGdeQXVyNjEwNTM2Mzc@._V1_SY150_CR3,0,101,150_.jpg', 
+# 'imdbID': '9223870', 
+# 'languages': ['English'], 
+# 'title': 'Woke', 
+# 'year': 2020, 
+# 'kind': 'tv series', 
+# 'series years': '2020-', 
+# 'akas': ['Прозревший (Russia)'], 
+# 'seasons': ['1'], 
+# 'writer': [<Person id:10056664[http] name:_Keith Knight_>, 
+# <Person id:1187871[http] name:_Marshall Todd_>, 
+# <Person id:2953647[http] name:_Conor Galvin_>], 
+# 'production companies': [<Company id:0294149[http] name:_Cloud Nine Productions_>, 
+# <Company id:0219609[http] name:_Olive Bridge Entertainment_>, 
+# <Company id:0086397[http] name:_Sony Pictures Television_>], 
+# 'distributors': [<Company id:0218858[http] name:_Hulu_>], 
+# 'special effects': [<Company id:0197392[http] name:_Ingenuity Studios_>], 
+# 'other companies': [<Company id:0632193[http] name:_Bespoke Post_>, 
+# <Company id:0028470[http] name:_Company 3_>], 
+# 'plot': ['Keef is an African-American cartoonist on the verge of mainstream success when an unexpected incident changes his life.::Anonymous']}
